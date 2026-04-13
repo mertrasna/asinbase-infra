4abe79b55fbdb84b3ab1a8e4902a77b025b237bc
Mert Rasna <mrasnalu02@gmail.com>
2026-04-13 13:29:02 +0200

    feat: IAM role and policy attachment for reading SSM for EC2

---
+++ environments/dev/iam.tf
@@ -0,0 +1,48 @@
+# The IAM role - an identity that AWS services can "assume". 
+resource "aws_iam_role" "ec2_dev" {
+  name = "dev-ec2-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      }
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+
+# Policy attachment - defines what role is allowed to do
+# Attach the AWS-managed SSM policy (enables Session Manager)
+resource "aws_iam_role_policy_attachment" "ssm_core" {
+  role = aws_iam_role.ec2_dev.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+}
+
+# Custom policy to read your SSM parameters
+resource "aws_iam_role_policy" "read_ssm_params" {
+  name = "dev-read-ssm-params"
+  role = aws_iam_role.ec2_dev.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Action = [
+        "ssm:GetParameter",
+        "ssm:GetParameters",
+        "ssm:GetParametersByPath"
+      ]
+      # Scope this to just your dev parameters — don't give access to all SSM params
+      Resource = "arn:aws:ssm:*:*:parameter/dev/*"
+    }]
+  })
+}
+
+# The instance profile - a wrapper around the role. EC2 can't attach a role directly. It attaches and instance profile
+resource "aws_iam_instance_profile" "ec2_dev" {
+  name = "dev-ec2-instance-profile"
+  role = aws_iam_role.ec2_dev.name
+}

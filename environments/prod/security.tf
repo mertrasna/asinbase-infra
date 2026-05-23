dc7d2926c245ae7c6136e2dacd3dc3840eebd868
Mert Rasna <mrasnalu02@gmail.com>
2026-05-23 23:09:28 +0200

    feat(prod): new sg and rules for alb

--- environments/prod/security.tf
+++ environments/prod/security.tf
@@ -8,6 +8,17 @@ resource "aws_security_group" "web" {
   }
 }
 
+# Security group for the Application Load Balancer
+resource "aws_security_group" "alb" {
+  name        = "prod-alb-sg"
+  description = "Prod ALB: HTTP/HTTPS from anywhere"
+  vpc_id      = aws_vpc.main.id
+
+  tags = {
+    Name = "${local.name_prefix}-alb-sg"
+  }
+}
+
 # Ingress rule
 resource "aws_vpc_security_group_ingress_rule" "ssh_public" {
   security_group_id = aws_security_group.web.id
@@ -20,6 +31,35 @@ resource "aws_vpc_security_group_ingress_rule" "ssh_public" {
   lifecycle {
     create_before_destroy = true
   }
+}
+
+# will redirect to HTTPS at the listener  
+resource "aws_vpc_security_group_ingress_rule" "alb_http_public" {
+  security_group_id = aws_security_group.alb.id
+  ip_protocol       = "tcp"
+  description       = "HTTP from anywhere"
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 80
+  to_port           = 80
+}
+
+resource "aws_vpc_security_group_ingress_rule" "alb_https_public" {
+  security_group_id = aws_security_group.alb.id
+  ip_protocol       = "tcp"
+  description       = "HTTPS from anywhere"
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 443
+  to_port           = 443
+}
+
+# Allow traffic from the ALB to reach the EC2 on port 80
+resource "aws_vpc_security_group_ingress_rule" "http_from_alb" {
+  security_group_id            = aws_security_group.web.id
+  ip_protocol                  = "tcp"
+  description                  = "HTTP from ALB only"
+  from_port                    = 80
+  to_port                      = 80
+  referenced_security_group_id = aws_security_group.alb.id
 }
 
 resource "aws_vpc_security_group_ingress_rule" "http_public" {
@@ -44,6 +84,13 @@ resource "aws_vpc_security_group_ingress_rule" "https_public" {
 resource "aws_vpc_security_group_egress_rule" "all_out" {
   security_group_id = aws_security_group.web.id
   ip_protocol       = "-1" # -1 means any protocol
+  description       = "Allow all outbound"
+  cidr_ipv4         = "0.0.0.0/0"
+}
+
+resource "aws_vpc_security_group_egress_rule" "alb_all_out" {
+  security_group_id = aws_security_group.alb.id
+  ip_protocol       = "-1"
   description       = "Allow all outbound"
   cidr_ipv4         = "0.0.0.0/0"
 }
